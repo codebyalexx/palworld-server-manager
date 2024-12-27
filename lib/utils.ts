0fe4508aa504ae51cae48abfb12fbf4d0da82a69
codebyalexx <a.legal.contact@gmail.com>
2024-12-28 00:28:53 +0100

    Added discord webhook

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function sendDiscordMsg(message: string) {
+  fetch(
+    "https://discord.com/api/webhooks/1322344934602178652/vEWhBUHeSsQ83uxMOWmzC-A_40ryw2W6Dcz8fJZBS54388qdY6Pzo9h4LRI1rLyKXwlG",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: message,
+      }),
+    }
+  );
 }
